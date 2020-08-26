b8e65c517f431a3d93037414b43c4ea314b5e491
Henrique Faria <henriquesdfaria@gmail.com>
2020-08-25 21:29:13 -0300

    required keycloak

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Keycloak from "keycloak-js";
 
 function App() {
+
+  const keycloakConfig = {
+    url: "http://localhost:8080/auth/",
+    realm: "demo",
+    clientId: "react-app-keycloak"
+  }
+  const keycloak = new Keycloak(keycloakConfig)
+
+  keycloak.init({
+    onLoad: "login-required"
+  }).then((response) => {
+    console.log("success");
+    console.log(response);
+  }).catch(reason => console.log(reason));
+
   return (
     <div className="App">
       <header className="App-header">
